590c6c6adf93d953346fe26f93eb2646106315ee
biones <iijimasat@gmail.com>
2021-03-16 22:02:37 +0900

    fix

--- twitterbot.py
+++ twitterbot.py
@@ -23,11 +23,13 @@ def getApiInstance():
 import numpy as np
 import numpy as np
 
+import numpy as np
+import re
 
 def search(search_query):
-
+    global Ntweet
+    print(Ntweet)
     tweet=tweepy.Cursor(api.search, q =search_query,  include_entities = True, tweet_mode = 'extended', lang = 'ja').items(Ntweet)    
-
     #tweet=api.search(search_query,count=5000)
 
     texts=[]
@@ -54,8 +56,9 @@ def run_bot(dat):
         time.sleep(ST)
 
 def retweetWithComment(search_query,d,tweets,texts=[]):
+    global gtest
     cnt=0
-    print(d)
+    #print(d)
     for i in range(len(tweets)):
         if "notrandom" in d:
             print(notrand)
@@ -67,7 +70,7 @@ def retweetWithComment(search_query,d,tweets,texts=[]):
             tw=tweets[k]
             
         print("text",texts[k])
-        
+        print(i,gtest)
         #m.min_retweet
         try:
             if tw.retweet_count<=d["min_retweet"]:
@@ -87,51 +90,81 @@ def retweetWithComment(search_query,d,tweets,texts=[]):
         else:
             locstr=""
         
+        
+        try:
+            tweet=np.random.choice(d["tweet"],1)[0]
+        except:
+            tweet=d["tweet"]
+            
         if "pickup" in d:     
-            ptweet=search_query+"を利用中の"+locstr+u.name[:20]+" @"+u.screen_name[:20]+"さん "+d["tweet"]
+            ptweet=search_query+"を利用中の"+locstr+u.name[:20]+" @"+u.screen_name[:20]+"さん "+tweet
         else:
-            ptweet=d["tweet"]
-        ptweet=ptweet[:140]
+            ptweet=tweet            
+        
+        if re.match("https?://[\w/:%#\$&\?\(\)~\.=\+\-]+",ptweet):            
+            ptweet=ptweet[:135]
+        else:
+            ptweet=ptweet[:140]
+        
         print(ptweet)
         print("strlength",len(ptweet))
+        
         ptweet+=" "+urls        
 
-
         try:
-            print(1)
-            #api.update_status(ptweet)
+            if gtest:
+                print(1)
+            else:          
+                print("bpost")
+                api.update_status(ptweet)
+                print("posted")
+            cnt+=1
         except:
             continue
-        cnt+=1
         try:
-            print(cnt,d["ntry"])
+            s=d["ntry"]
         except:
-            pass
-        if "ntry" not in d:
-            print("retun")
             return
-        elif cnt>=d["ntry"]:
+        
+        print("NNNNoret",test,i,s)
+        input()
+        
+        if cnt>=d["ntry"]:
             return True
+        
+        
          
         time.sleep(int(ST*0.1))
-    
 
-ST=300
-Ntweet=1000
+
+        
+gtest=False
+
+
+if gtest:
+    ST=10
+else:
+    ST=30
+
+Ntweet=int(ST*3)
     
 cm="それは税金の無駄ですね #A型事業所 #B型事業所 #就労移行支援 などの悪質事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になります。自宅で自習や内職をしましょう"
 settai="厚労省と福祉の業者団体でも絶対こういうのありますね、全国社会就労センター協議会,きょうされん,全国就労移行支援事業所連絡協議会全国就業センターとか　"+"https://fukusiprob.blogspot.com/2021/03/blog-post_8.html #接待 #福祉ビジネス"
 
 dat={
-    "放課後デイサービス":{"tweet":"LINEで体温の報告を聞いりアンパンマンを見せるだけで1万数千円も出るヤツですね、税金の無駄です。利用を控えてください　#放課後デイサービス #福祉ビジネス ",
+    "放課後デイサービス":{"tweet":"LINEで体温の報告を聞いりアンパンマンを見せるだけで1万数千円も出るヤツですね、税金の無駄です。利用を控えてくださいhttps://bit.ly/2OSOlWV　#放課後デイサービス #福祉ビジネス ",
                  "pickup":True},
     
     "A型事業所":{
-        "tweet":"それは税金の無駄ですね掃除や家事の報告で１万円の報酬が出る #A型事業所 #B型事業所などの茶番事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になりアホになります。自習や内職をしましょう",
+        "tweet":[
+            "それは税金の無駄ですね掃除や家事の報告で１万円の報酬が出る #A型事業所 #B型事業所などの茶番事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になりアホになります。自習や内職をしましょう",
+            "それは税金の無駄ですね掃除や家事の報告で１万円の報酬が出る #A型事業所 #B型事業所などの茶番事業の利用は控えてください  https://bit.ly/3tte9HZ"],
         "pickup":True        
     },
     "就労移行支援":{
-        "tweet":"エクセルやUdemyでの自習で1.5万円/日の報酬が出る #就労移行支援 などの茶番事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になります。ハロワの基金訓練などを探しましょう",
+        "tweet":
+            ["エクセルやUdemyの自習で1.5万円/日の報酬が出る #就労移行支援 などの茶番事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になります。ハロワの基金訓練などを探しましょう",
+            s2],
         "pickup":True 
         
     },
