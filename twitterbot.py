f6ef4bd66906b028c990ed1f3070cfdd3e6dc6d4
biones <iijimasat@gmail.com>
2021-03-20 20:19:21 +0900

    fix

--- twitterbot.py
+++ twitterbot.py
@@ -20,11 +20,11 @@ def getApiInstance():
     api = tweepy.API(auth, wait_on_rate_limit = True)
     return api
 
-import numpy as np
-import numpy as np
 
+import re
 import numpy as np
 import re
+import sqlite3
 
 def search(search_query):
     global Ntweet
@@ -49,25 +49,34 @@ def run_bot(dat):
     while True:
         key=np.random.choice(list(dat.keys()),1)[0]
         tweets,texts=search(key)          
-        print(key)
+        #print(key)
         retweetWithComment(key,dat[key],tweets,
-                           texts=texts)
-        
+                           texts=texts)        
         time.sleep(ST)
 
+def insertusertable(r):
+    u=r.user
+    dd=[u.id,u.screen_name,u.name,search_query,u.description,r.full_text,u.location]    
+    cur.execute("insert into users(id,screen_name,name,search_query,description,tweet,location) values(?,?,?,?,?,?,?)",dd)
+    
+def inserttweet(tw):
+    tw=[r.id,r.full_text,r.user.screen_name,r.user.name,r.user.description,search_query,r.created_at,r.user.location,r.retweet_count]
+    cur.execute("insert into tweets(id,tweet,screen_name,name,description,search_query,created_at,location,retweet_count) values(?,?,?,?,?,?,?,?,?)",tw)
+    
+
 def retweetWithComment(search_query,d,tweets,texts=[]):
-    global gtest
+    global gtest,cur
     cnt=0
     #print(d)
     for i in range(len(tweets)):
-        if "notrandom" in d:
-            print(notrand)
-            tw=df[i]            
-            k=i
-        else:            
+        #if "notrandom" in d:
+          #  print(notrand)
+           # tw=df[i]            
+            #k=i
+        #else:            
             #tw=np.random.choice(tweets,1)[0]
-            k=np.random.choice(range(len(tweets)),1)[0]
-            tw=tweets[k]
+        k=np.random.choice(range(len(tweets)),1)[0]
+        tw=tweets[k]
             
         print("text",texts[k])
         print(i,gtest)
@@ -83,21 +92,37 @@ def retweetWithComment(search_query,d,tweets,texts=[]):
         #print(u)
         urls="https://twitter.com/"+u.screen_name+"/status/"+str(tw.id)    
         
-        if len(u.location)>0:
-            locstr=u.location+"の"
-            locstr=locstr.replace("日本","")
+        if len(u.location)>0:            
+            locstr=u.location.replace("日本","")
             locstr=locstr[:12]
+            locstr=locstr+"の"
         else:
             locstr=""
         
-        
+        #q="insert into users(screen_name,name,profile,location) values"+blacket([u.screen_name,u.name,u.description,u.location])
+        #print(q)
+        #dd=(u.screen_name,u.name,u.description,u.location)
+        #dd=[r.user.id,r.user.screen_name,r.user.name,search_query,r.user.description,r.full_text,r.user.location]
+        #print(dd)
+        try:
+            insertusertable(u)
+            #cur.execute("insert into users(screen_name,name,description,location) values(?,?,?,?)",dd)            
+            conn.commit()            
+        except:
+            pass
+        try:
+            inserttweet(tw)
+        except:
+            pass
+            
+
         try:
             tweet=np.random.choice(d["tweet"],1)[0]
         except:
             tweet=d["tweet"]
             
         if "pickup" in d:     
-            ptweet=search_query+"を利用中の"+locstr+u.name[:20]+" @"+u.screen_name[:20]+"さん "+tweet
+            ptweet="#"+search_query+" を利用中の"+locstr+u.name[:20]+" @"+u.screen_name[:20]+"さん "+tweet
         else:
             ptweet=tweet            
         
@@ -126,8 +151,8 @@ def retweetWithComment(search_query,d,tweets,texts=[]):
         except:
             return
         
-        print("NNNNoret",test,i,s)
-        input()
+        print("NNNNoret",cnt,gtest,i,d["ntry"])
+        #input()
         
         if cnt>=d["ntry"]:
             return True
@@ -144,27 +169,36 @@ gtest=False
 if gtest:
     ST=10
 else:
-    ST=30
+    ST=1200
 
-Ntweet=int(ST*3)
+Ntweet=int(ST)
     
 cm="それは税金の無駄ですね #A型事業所 #B型事業所 #就労移行支援 などの悪質事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になります。自宅で自習や内職をしましょう"
 settai="厚労省と福祉の業者団体でも絶対こういうのありますね、全国社会就労センター協議会,きょうされん,全国就労移行支援事業所連絡協議会全国就業センターとか　"+"https://fukusiprob.blogspot.com/2021/03/blog-post_8.html #接待 #福祉ビジネス"
 
+stop='''【悪質な事業所は許せません】
+ここの事業所はどうなんだろう？アンケートご協力お願いします(匿名可) → http://bit.ly/3vIZ9HW 
+    STOP！税金の無駄・不正 #就労移行支援　#A型事業所　#放課後デイ　#福祉ビジネス'''
+
 dat={
-    "放課後デイサービス":{"tweet":"LINEで体温の報告を聞いりアンパンマンを見せるだけで1万数千円も出るヤツですね、税金の無駄です。利用を控えてくださいhttps://bit.ly/2OSOlWV　#放課後デイサービス #福祉ビジネス ",
+    "放課後デイサービス":{"tweet":["LINEで体温の報告を受けたり,アンパンマンを見せるだけで1.5万円も出る茶番事業ですね、税金の無駄です。利用を控えてくださいhttps://bit.ly/2OSOlWV　#放課後デイ #福祉ビジネス",
+                          stop,
+                         "税金ジャブジャブ障害福祉サービスの利用は控えましょう　https://bit.ly/2OSOlWV　放課後デイ #福祉ビジネス"],
                  "pickup":True},
     
     "A型事業所":{
         "tweet":[
-            "それは税金の無駄ですね掃除や家事の報告で１万円の報酬が出る #A型事業所 #B型事業所などの茶番事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になりアホになります。自習や内職をしましょう",
-            "それは税金の無駄ですね掃除や家事の報告で１万円の報酬が出る #A型事業所 #B型事業所などの茶番事業の利用は控えてください  https://bit.ly/3tte9HZ"],
+            "税金の無駄ですね掃除や家事の報告で１万円の報酬が出る茶番事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になりアホになります。自習や内職をしましょう https://bit.ly/3tte9HZ",
+            "それは税金の無駄ですね掃除や家事の報告で１万円の報酬が出る茶番事業の利用は控えてください  https://bit.ly/3tte9HZ #B型事業所",
+            stop],
         "pickup":True        
     },
     "就労移行支援":{
         "tweet":
-            ["エクセルやUdemyの自習で1.5万円/日の報酬が出る #就労移行支援 などの茶番事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になります。ハロワの基金訓練などを探しましょう",
-            s2],
+            ["エクセルやUdemyの自習で1.5万円/日の報酬が出る茶番事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になります。ハロワの基金訓練などを探しましょうhttps://bit.ly/3bUqaQR",
+             "エクセルやUdemyの自習で1.5万円/日の報酬が出る茶番事業の利用は控えてくださいマトモな事に税金が使われなくなり障害者の迷惑になります　https://bit.ly/3bUqaQR ",
+             stop
+            ],
         "pickup":True 
         
     },
@@ -175,5 +209,14 @@ dat={
     }    
 }
 
+dbname = 'fukusi.db'
+conn = sqlite3.connect(dbname)
+cur= conn.cursor()
+#cur.execute(
+#    'CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT,        
+#     screen_name STRING unique)')
+
 run_bot(dat)
 
+conn.close()
+            
